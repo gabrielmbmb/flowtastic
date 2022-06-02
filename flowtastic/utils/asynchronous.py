83b27ed33d8dc91c2813e1272786dda4e7fd4437
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-06-02 22:01:29 +0200

    :sparkles: Add `wrapper_func` parameter

--- flowtastic/utils/asynchronous.py
+++ flowtastic/utils/asynchronous.py
@@ -5,17 +5,24 @@ from typing import Any, Awaitable, Callable
 
 
 def create_gathering(
-    funcs: list[Callable[..., Awaitable[Any]]], *args: Any, **kwargs: Any
+    funcs: list[Callable[..., Awaitable[Any]]],
+    wrapper_func: Callable[..., Awaitable[None]] | None = None,
+    *args: Any,
+    **kwargs: Any,
 ) -> asyncio.futures.Future:  # type: ignore
     """Create a gathering from a list of coroutines. The functions are called with the provided
     arguments and keyword arguments.
 
     Args:
         funcs: List of coroutines.
+        wrapper_func: Wrapper function that will wrap each of the coroutines in `funcs`.
         *args: Positional arguments to pass to the coroutines.
         **kwargs: Keyword arguments to pass to the coroutines.
 
     Returns:
         A gathering coroutine.
     """
-    return asyncio.gather(*[func(*args, **kwargs) for func in funcs])
+    if wrapper_func is not None:
+        return asyncio.gather(*[wrapper_func(func, *args, **kwargs) for func in funcs])
+    else:
+        return asyncio.gather(*[func(*args, **kwargs) for func in funcs])
