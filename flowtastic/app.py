400bf6b99fa8e500f57add2c6e9669828d9e0712
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-06-02 22:06:32 +0200

    :sparkles: Add deserialization with no `BaseModel`

--- flowtastic/app.py
+++ flowtastic/app.py
@@ -9,6 +9,8 @@ from flowtastic._deserialization_combo import _DeserializationCombo
 from flowtastic.logger import get_logger
 from flowtastic.message import DeserializationError, Message
 from flowtastic.types import DeserializationErrorFunc, ValidationErrorFunc
+from flowtastic.utils.asynchronous import create_gathering
+from flowtastic.utils.pydantic_models import is_base_model_subclass
 from pydantic import BaseModel, ValidationError
 
 if TYPE_CHECKING:
@@ -33,9 +35,11 @@ class FlowTastic:
     _loop: asyncio.AbstractEventLoop
     _consume_task: asyncio.Task[None]
     _consumer: AIOKafkaConsumer
-    _topic_to_model: dict[str, list[_DeserializationCombo]] = defaultdict(list)
-    _model_to_subscriber: dict[
-        _DeserializationCombo, list[SubscriberFunc]
+    _topic_to_deserializer: dict[
+        str, list[Message | _DeserializationCombo]
+    ] = defaultdict(list)
+    _deserializer_to_subscriber: dict[
+        _DeserializationCombo | Message, list[SubscriberFunc]
     ] = defaultdict(list)
 
     def __init__(self, name: str, broker: str) -> None:
@@ -49,29 +53,23 @@ class FlowTastic:
         self.broker = broker
         self._loop = asyncio.get_event_loop()
 
-    def _register_deserialization_combo(
+    def _register_deserializer(
         self,
         topic: str,
         func: SubscriberFunc,
-        message: Message,
-        pydantic_base_model: type[BaseModel],
+        deserializer: Message | _DeserializationCombo,
     ) -> None:
-        """Register a `Message` class and a `BaseModel` class to the `_topic_to_model`
-        dictionary.
+        """Register a `Message` class or a `_DeserializationCombo` to the `_topic_to_deserializer`
+        dictionary and the subscriber function to the `_deserializer_to_subscriber` dictionary.
 
         Args:
-            topic: The topic to register the `Message` class and the `BaseModel` class to.
+            topic: The topic to which `deserializer` should be registered.
             func: The subscriber function to register.
-            message: The `Message` class instance.
-            pydantic_base_model: The `BaseModel` class type.
+            deserializer: The `Message` class or the `_DeserializationCombo` class.
         """
-        deserialization_combo = _DeserializationCombo(
-            message=message,
-            pydantic_base_model=pydantic_base_model,
-        )
-        if deserialization_combo not in self._topic_to_model[topic]:
-            self._topic_to_model[topic].append(deserialization_combo)
-        self._model_to_subscriber[deserialization_combo].append(func)
+        if deserializer not in self._topic_to_deserializer[topic]:
+            self._topic_to_deserializer[topic].append(deserializer)
+        self._deserializer_to_subscriber[deserializer].append(func)
 
     def subscriber(
         self,
@@ -116,28 +114,30 @@ class FlowTastic:
                     f"using `@{self.__class__.__name__}.subscribe` method takes more than "
                     f"one argument."
                 )
+            # TODO: handle no annotations
             func_input_type = list(func.__annotations__.values())[0]
+            # TODO: handle default message if not provided
             func_input_message = cast(tuple[Any, ...], func.__defaults__)[0]
-            if issubclass(func_input_type, BaseModel):
-                self._register_deserialization_combo(
-                    topic=topic,
-                    func=func,
-                    message=func_input_message,
-                    pydantic_base_model=func_input_type,
+            if is_base_model_subclass(func_input_type):
+                logger.debug(
+                    f"Registering {func} as subscriber for '{topic}' topic with deserialization "
+                    f"of `{func_input_message}` to `{func_input_type}` `pydantic.BaseModel`"
+                )
+                deserializer = _DeserializationCombo(
+                    message=func_input_message, pydantic_base_model=func_input_type
                 )
             else:
-                pass
+                logger.debug(
+                    f"Registering {func} as subscriber for '{topic}' topic with deserialization "
+                    f"of `{func_input_message}`"
+                )
+                deserializer = func_input_message
+            self._register_deserializer(
+                topic=topic, func=func, deserializer=deserializer
+            )
             return func
 
         return register_subscriber
-
-    def publish(self, topic: str) -> None:
-        """A decorator to register a function as a publisher of a topic.
-
-        Args:
-            topic: The topic to publish to.
-        """
-        pass
 
     def _consumed_topics(self) -> list[str]:
         """Returns the topics that should be consumed by the application.
@@ -145,7 +145,7 @@ class FlowTastic:
         Returns:
             A list of topics to be consumed.
         """
-        return list(self._topic_to_model.keys())
+        return list(self._topic_to_deserializer.keys())
 
     def _create_consumer(self) -> None:
         """Creates an instance of `aiokafka.AIOKafkaConsumer` to consume messages from the
@@ -155,38 +155,55 @@ class FlowTastic:
         )
 
     async def _process_message(
-        self,
-        value: bytes,
-        message: Message,
-        pydantic_base_model: type[BaseModel] | None = None,
+        self, value: bytes, deserializer: Message | _DeserializationCombo
     ) -> BaseModel | Any | None:
-        """Process the `value` attribute of a `aiokafka.ConsumerRecord` first deserializing
-        the message and then converting it to a `pydantic.BaseModel` if needed. If the deserialization
-        fails or the conversion to a `pydantic.BaseModel` fails, then no error is raised
-        as it would cause the whole app to stop. Instead, a warning message is logged.
+        """Process the `value` attribute of a `aiokafka.ConsumerRecord` deserializing the
+        message and if needed, converting it to a `pydantic.BaseModel`. If the deserialization
+        fails or the conversion to a `pydantic.BaseModel` fails, then the provided callbacks
+        functions for each error are called.
 
         Args:
             value: The `value` attribute of the `aiokafka.ConsumerRecord`.
-            message: The message class to use for deserialization.
-            pydantic_base_model: The `pydantic.BaseModel` class to use for deserialization.
+            deserializer: A `Message` class to deserialize the `aiokafka.ConsumerRecord.value`
+                to a Python object or a `_DeserializationCombo` object to deserialize the
+                `aiokafka.ConsumerRecord.value` to a `pydantic.BaseModel`.
 
         Returns:
             If the deserialization and conversion to a `pydantic.BaseModel` succeed, then
             the deserialized message is returned. Otherwise, `None` is returned.
         """
         try:
-            deserialized = message.deserialize(value)
-            if pydantic_base_model:
-                return pydantic_base_model(**deserialized)
-            return deserialized
-        except DeserializationError as e:
-            logger.warning(f"Failed to deserialize message: {e}")
-        except ValidationError as e:
-            if pydantic_base_model:
-                logger.warning(
-                    f"Could not create '{pydantic_base_model.__name__}' from deserialized message: {e}"
-                )
+            # Just deserialize the message using the provided deserializer
+            if isinstance(deserializer, Message):
+                logger.debug(f"Deserializing message using `{deserializer}`...")
+                return deserializer.deserialize(value)
+            # We have a deserialization combo, so first deserialize the message and then
+            # convert it to the provided `pydantic.BaseModel` subclass.
+            logger.debug(
+                f"Deserializing message using `{deserializer.message}` and then converting "
+                f"it to `{deserializer.pydantic_base_model}`..."
+            )
+            message = deserializer.message.deserialize(value)
+            return deserializer.pydantic_base_model(**message)
+        except DeserializationError as e:  # noqa
+            # TODO: handle deserialization error using `Message.on_deserialization_error`
+            pass
+        except ValidationError as e:  # noqa
+            # TODO: handle deserialization error using `BaseModel.on_validation_error`
+            pass
         return None
+
+    async def _subscriber_wrapper(
+        self, func: SubscriberFunc, arg: BaseModel | Any
+    ) -> None:
+        """Wraps the subscriber function to handle the return value of the function if needed.
+
+        Args:
+            func: The subscriber function to wrap.
+            arg: The argument to pass to the subscriber function.
+        """
+        result = await func(arg)  # noqa
+        # TODO: handle result if the subscriber function has `Publish` annotation.
 
     async def _distribute_message(self, record: ConsumerRecord) -> None:
         """Distributes the message to the subscribers. For that, it first deserializes the
@@ -196,15 +213,21 @@ class FlowTastic:
             record: The `aiokafka.ConsumerRecord` to distribute.
         """
         topic = record.topic
-        for deserialization_combo in self._topic_to_model[topic]:
+        to_be_awaited = []
+        for deserializer in self._topic_to_deserializer[topic]:
             python_object = await self._process_message(
-                record.value,
-                deserialization_combo.message,
-                deserialization_combo.pydantic_base_model,
+                record.value, deserializer=deserializer
             )
-            if python_object:
-                for subscriber in self._model_to_subscriber[deserialization_combo]:
-                    await subscriber(python_object)
+            if not python_object:
+                continue
+            to_be_awaited.append(
+                create_gathering(
+                    self._deserializer_to_subscriber[deserializer],
+                    self._subscriber_wrapper,
+                    python_object,
+                )
+            )
+        asyncio.gather(*to_be_awaited)
 
     async def _consume(self) -> None:
         """Start consuming messages from the Kafka broker."""
@@ -212,6 +235,7 @@ class FlowTastic:
         await self._consumer.start()
         try:
             async for msg in self._consumer:
+                # TODO: create a thread for each topic consumed
                 await self._distribute_message(msg)
         finally:
             await self._consumer.stop()
@@ -222,6 +246,8 @@ class FlowTastic:
         try:
             self._loop.run_until_complete(self._consume_task)
         except asyncio.CancelledError:
+            pass
+        except KeyboardInterrupt:
             pass
         finally:
             self._loop.run_until_complete(self._consumer.stop())
