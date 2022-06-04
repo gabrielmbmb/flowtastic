3381d86f69a69bbab12c759616e7ee41984975bc
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-06-04 20:24:55 +0200

    :bookmark: Release version `0.0.1a0`

--- flowtastic/__init__.py
+++ flowtastic/__init__.py
@@ -5,7 +5,7 @@ from flowtastic.logger import configure_logger
 from flowtastic.message import JSONMessage, Message
 from flowtastic.publish import Publish
 
-__version__ = "0.0.1"
+__version__ = "0.0.1a0"
 
 __all__ = ["FlowTastic", "JSONMessage", "Message", "Publish"]
 
