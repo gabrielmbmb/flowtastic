694306ad292521d3648aa997cdd742aa6aaed7de
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-06-04 19:13:05 +0200

    :sparkles: Add `deserialize` implementation

--- flowtastic/message/json_message.py
+++ flowtastic/message/json_message.py
@@ -8,10 +8,24 @@ except ImportError:
 from typing import Any
 
 from flowtastic.message.base import Message
+from pydantic import BaseModel
 
 
 class JSONMessage(Message):
     """A message that is encoded as JSON."""
+
+    def _serialize(self, python_object: BaseModel | Any) -> str:
+        """Serialize the message from a Python object to a JSON string.
+
+        Args:
+            python_object: The Python object to serialize.
+
+        Returns:
+            The serialized message.
+        """
+        if isinstance(python_object, BaseModel):
+            return python_object.json()
+        return json.dumps(python_object)  # type: ignore
 
     def _deserialize(self, message: str) -> Any:
         """Deserialize the message from a JSON string to a Python object.
