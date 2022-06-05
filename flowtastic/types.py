b5adad56f49fc0ef1a9db494db44a982e7f3b0c9
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-06-05 11:37:44 +0200

    :bug: Fix `dict` is not subscriptable (python<=3.8)

--- flowtastic/types.py
+++ flowtastic/types.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-from typing import Any, Awaitable, Callable, Union
+from typing import Any, Awaitable, Callable, Dict, Union
 
 from flowtastic.message import DeserializationError
 from flowtastic.publish import Publish
 from pydantic import BaseModel, ValidationError
 
 SubscriberFunc = Callable[
-    [Union[BaseModel, dict[str, Any]]], Awaitable[Union[Publish, None]]
+    [Union[BaseModel, Dict[str, Any]]], Awaitable[Union[Publish, None]]
 ]
 """The signature of the function that is decorated using `flowtastic.FlowTastic.subscriber`
 decorator."""
